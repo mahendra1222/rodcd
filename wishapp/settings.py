6d3d65df799a76bedbfa70e89d66476c72e88c13
mahenndra1222 <gangasanimahenndra92@gmail.com>
2025-05-11 17:20:26 +0530

    sqllite222

--- wishapp/settings.py
+++ wishapp/settings.py
@@ -74,10 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wishapp.wsgi.application'
 
-
-Database
-https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
