d4ceaf2301164cf2a7210ef08192241a9ddd94ae
mahenndra1222 <gangasanimahenndra92@gmail.com>
2025-05-13 06:00:10 +0530

    added some more message

--- greetings/views.py
+++ greetings/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>🎉 Happy New Year! 🎉 Mahendra Reddy Gangasani Great to here from you again buddy</h1>")
+    return HttpResponse("<h1>🎉 Happy New Year! 🎉 Mahendra Reddy Gangasani Great to here from you again buddy  your fther name is Krishna reddy</h1>")
 
 
 
